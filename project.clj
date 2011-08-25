066669f8b555e6e45733926217322b295ff91873
Stefan Hübner <sthuebner@googlemail.com>
2011-08-25 21:29:11 +0200

    Basic Leiningen setup

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject soundcloud-challenge "1-SNAPSHOT"
+  :description "sthuebner's solution to SoundCloud's dev challenge"
+
+  :dependencies [[org.clojure/clojure "1.2.1"]
+		 [org.clojure/clojure-contrib "1.2.0"]])
